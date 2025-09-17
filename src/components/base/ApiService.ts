3500908f0c389b4f8c70ae05a52f3cc228a8cbbf
Asumodeusu <pop.illya@yandex.ru>
2025-09-17 17:09:59 +0500

    extends

--- src/components/base/ApiService.ts
+++ src/components/base/ApiService.ts
@@ -3,14 +3,15 @@ import { IProduct, IOrder, IOrderResult } from "../../types";
 
 export class ApiService extends Api {
   constructor(baseUrl: string, options: RequestInit = {}) {
-    super(baseUrl, options); // добавил инциализацию
+    super(baseUrl, options); // инициализация конструктора родительского класса
   }
+  /*Если позволите добавить - то там в тз где-то было написано не использовать екстеденс - поэтому как-то и не подумал)*/
 
   getProducts(): Promise<IProduct[]> {
-    return this.get<IProduct[]>("/product/");
+    return this.get("/product/");
   } // получение массива товаров
 
   createOrder(order: IOrder): Promise<IOrderResult> {
-    return this.post<IOrderResult>("/order/", order);
+    return this.post("/order/", order);
   } // отправка заказа
 }
